9924c17b8194fefe22f5afbb4f02da3f69b06357
Adam <ajpoundall@gmail.com>
2019-12-08 18:06:34 +0000

    Now added second routing to see entire database after submit!

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import forms
+from .models import people
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -8,9 +10,15 @@ def index(request):
 		form = forms.CreatePerson(request.POST)
 		if form.is_valid():
 			form.save()
+			return redirect('overview')
 
 
 	else:
 		form = forms.CreatePerson()
 
 	return render(request, 'index.html', {'form':form})
+
+def overview(request):
+	peoples = people.objects.all()
+	args = {"people": peoples}
+	return render(request, 'overview.html', args)
